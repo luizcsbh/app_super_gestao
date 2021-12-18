8f3870cf56b942643da34be94e2469d803d6aca7
luizc.lightbase <luizc.lightbase@sebraemg.com.br>
2021-12-18 15:06:51 -0300

    Criação da view teste

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,6 +8,7 @@ class TesteController extends Controller
 {
     public function teste(int $p1, int $p2)
     {
-        echo "A soma de $p1 + $p2 é: ".($p1+$p2);
+        //echo "A soma de $p1 + $p2 é: ".($p1+$p2);
+        return view();
     }
 }
