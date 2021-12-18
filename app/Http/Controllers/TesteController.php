8ebd5a517efd734275397d14f031e36949b5113e
luizc.lightbase <luizc.lightbase@sebraemg.com.br>
2021-12-18 15:26:42 -0300

    array with

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -12,6 +12,8 @@ class TesteController extends Controller
 
         //return view('site.teste', ['p1' => $p1, 'p2' => $p2]); //array associativo
 
-        return view('site.teste', compact('p1', 'p2')); //compact
+        //return view('site.teste', compact('p1', 'p2')); //compact
+
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2); //with
     }
 }
