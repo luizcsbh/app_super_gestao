debd20076e03bf3466fe9820fbdb2be01c99a9db
luizc.lightbase <luizc.lightbase@sebraemg.com.br>
2021-12-18 15:17:04 -0300

    Array associativo

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -9,6 +9,6 @@ class TesteController extends Controller
     public function teste(int $p1, int $p2)
     {
         //echo "A soma de $p1 + $p2 é: ".($p1+$p2);
-        return view('site.teste');
+        return view('site.teste', ['x' => $p1, 'y' => $p2]);
     }
 }
