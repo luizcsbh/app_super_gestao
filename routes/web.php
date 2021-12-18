9757e352a1b5360a6146cd652bc076a98a005030
luizc.lightbase <luizc.lightbase@sebraemg.com.br>
2021-12-18 15:38:35 -0300

    Criando Fonecedor Controller

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/login', function(){ return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores', 'FornecedorerController@index')->name('app.fornecedores');
     Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');    
 });
 
