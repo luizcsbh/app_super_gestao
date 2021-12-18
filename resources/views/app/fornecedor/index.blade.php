92052f1c5cd7f8f884e9b814dea2bd95ab3604e9
luizc.lightbase <luizc.lightbase@sebraemg.com.br>
2021-12-18 17:09:59 -0300

    usando unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,23 +2,22 @@
 
 @php
 /*
-    if() {
-
-    } elseif() {
-
-    } else {
-
-    }
+    if(!<condicao>) {} //enquanto executa se o retornofor true
+  
 */
-
 @endphp
 
-{{-- @dd($fornecedores) --}}
+{{-- @unless execute se o retorno for falso --}}
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados.</h3>
-@elseif(count($fornecedores >10 ))
-    <h3>Existem vários fornecedores cadastrados.</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados.</h3>
+Forncedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@if( !($fornecedores[0]['status'] == 'S') )
+        Fornecedor inativo
 @endif
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
+    Fornecedor inativo
+@endunless
+<br>
