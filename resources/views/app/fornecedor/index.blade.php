dc9ad2885f9630492574eb62f53c9f333c9110b7
luizc.lightbase <luizc.lightbase@sebraemg.com.br>
2021-12-18 15:58:14 -0300

    Comentarios

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,10 @@
 <h3>Fornecedores</h3>
 
 {{-- Fica um comentário que será descartado pelo interpretador do blade --}}
+
+@php
+    // Para cometário de uma linha
+    /*
+        Para cometários de multiplas linhas
+    */
+@endphp
